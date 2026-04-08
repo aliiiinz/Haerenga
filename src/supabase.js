1ecb7aa1cc76d980d0e0aa25caeeb9758e7e22dd
alastair weir <aliw@alastairs-MacBook-Pro.local>
2026-04-08 13:50:46 +1200

    fix supabase key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://smbctkyjtmyzxuweebxt.supabase.co'
-const supabaseKey = 'sb_publishable__dO48V7mTB5cmKN38qDVDA_S6umw_5uUR_PUBLISHABLE_KEY_HERE'
+const supabaseKey = 'sb_publishable__dO48V7mTB5cmKN38qDVDA_S6umw_5u'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
